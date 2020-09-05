422a2aa3837420f78d0c2e92d0bd40e74d22691e
mason parker <masongparker@gmail.com>
2020-09-04 21:45:38 -0400

    MVP,Going to attempt stretch.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-import SavedList from './Movies/SavedList';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory,
+} from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
+  const history = useHistory();
+  const Home = () => {
+    history.push("/");
+  };
+
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies')
-        .then(response => {
+        .get("http://localhost:5000/api/movies")
+        .then((response) => {
           setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
-
-  const addToSavedList = id => {
+  console.log(movieList);
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+      <SavedList
+        home={Home}
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
+
+      <Switch>
+        <Route path="/movies/:id">
+          <Movie movies={movieList} />
+        </Route>
+
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 };
